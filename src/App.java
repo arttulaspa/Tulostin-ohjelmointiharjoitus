df08d2764efcc13f00bb1d7d91203d8e6c2adf0a
Arttu Läspä <arttu.laspa@gmail.com>
2024-10-10 14:29:10 +0300

    Koodin muokkaaminen hieman loogisemmaksi
    Muuttujien määrityksen siirretty koodin lopusta lähemmäs alkua, jotta se olisi helpompi ymmärtää.

--- src/App.java
+++ src/App.java
@@ -17,14 +17,8 @@ public class App {
          // Luodaan kolmas muuttuja nimeltä tulo ja kerrotaan luku1 ja luku2 ja sijoitetaan tulos tulo-muuttujaan.
          double tulo;
          tulo = luku1*luku2;
-  
-         System.out.println("Luku1-muuttujan arvo on " + luku1);
-         System.out.println("Luku2-muuttujan arvo on " + luku2);
-  
-          // Tulostetaan konsoliin 5*2=10 jos luku1 on 5 ja luku2 on 2.
-         System.out.println(luku1 + "*" + luku2 + "=" + tulo);
 
-        //Määritetään summa, jako ja erotus muuttujat. Määritin muuttujat double-tyyppisiksi, koska tuloksessa voi olla desimaaleja.
+         //Määritetään summa, jako ja erotus muuttujat. Määritin kaikki muuttujat double-tyyppisiksi, koska tuloksessa voi olla desimaaleja.
          double summa;
          double jako;
          double erotus;
@@ -32,6 +26,12 @@ public class App {
          summa = luku1 + luku2;
          jako = luku1/luku2;
          erotus = luku1-luku2; 
+  
+         System.out.println("Luku1-muuttujan arvo on " + luku1);
+         System.out.println("Luku2-muuttujan arvo on " + luku2);
+  
+          // Tulostetaan konsoliin 5*2=10 jos luku1 on 5 ja luku2 on 2.
+         System.out.println(luku1 + "*" + luku2 + "=" + tulo);
  
     }
 }
