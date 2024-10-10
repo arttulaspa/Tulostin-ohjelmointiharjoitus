b622a61809d783820d9adf6c0666bc4720948c1f
Arttu Läspä <arttu.laspa@gmail.com>
2024-10-10 14:12:32 +0300

    Jatkoa koodille
    Alustetaan kaksi numeerista muuttujaa luku1, luku2 ja annetaan niille arvot. Luodaan kolmas muuttuja nimeltä tulo ja kerrotaan luku1 ja luku2 ja sijoitetaan tulos tulo-muuttujaan. Tulostetaan konsoliin 5*2=10 jos luku1 on 5 ja luku2 on 2.

--- src/App.java
+++ src/App.java
@@ -8,8 +8,21 @@ public class App {
 
         // Ohjelma alkaa tulostamalla "Hei olen Tulostin-ohjelma" ja "Ohjelman tekijä".
         // Ohjelmassa on myös määritetty String-tyyppinen muuttuja nimeltä tekijä ja tulostettu se konsoliin.
-
  
+
+         // Alustetaan kaksi numeerista muuttujaa luku1, luku2 ja annetaan niille arvot.
+         double luku1 = 5;
+         double luku2 = 2;
+  
+         // Luodaan kolmas muuttuja nimeltä tulo ja kerrotaan luku1 ja luku2 ja sijoitetaan tulos tulo-muuttujaan.
+         double tulo;
+         tulo = luku1*luku2;
+  
+         System.out.println("Luku1-muuttujan arvo on " + luku1);
+         System.out.println("Luku2-muuttujan arvo on " + luku2);
+  
+          // Tulostetaan konsoliin 5*2=10 jos luku1 on 5 ja luku2 on 2.
+         System.out.println(luku1 + "*" + luku2 + "=" + tulo);
  
     }
 }
