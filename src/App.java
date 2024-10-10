fea92f39466105b51c1cf8e1eb61ae83571d44de
Arttu Läspä <arttu.laspa@gmail.com>
2024-10-10 13:19:50 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.print("Ohjelman tekijä:");
+ 
+        String tekijä = "Arttu";
+        System.out.println(tekijä);
+
+        // Ohjelma alkaa tulostamalla "Hei olen Tulostin-ohjelma" ja "Ohjelman tekijä".
+        // Ohjelmassa on myös määritetty String-tyyppinen muuttuja nimeltä tekijä ja tulostettu se konsoliin.
+
+ 
+ 
+    }
+}
